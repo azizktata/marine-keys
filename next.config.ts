78369772ff658da0070a8a9a4d6b9aca1a78bf31
azizktata <65952489+azizktata@users.noreply.github.com>
2025-01-02 15:34:32 +0100

    updates

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,11 @@ const nextConfig = {
           {
             protocol: 'http' as const,
             hostname: 'marines.cluster027.hosting.ovh.net',
+  
+          },
+          {
+            protocol: 'https' as const,  // Allow https protocol
+            hostname: 'marines.cluster027.hosting.ovh.net',
             pathname: '/wp-content/uploads/**',
           },
           { protocol: "https" as const, hostname: "placehold.co" },
